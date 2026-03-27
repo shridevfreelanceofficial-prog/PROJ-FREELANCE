c11114bb8f92b18657c7e3e595f603d010bea2c9
shrikesh <shrikesh123shetty@gmail.com>
2026-03-27 17:53:55 +0530

    Improved ui and added more component

--- src/components/SplineBackground.tsx
+++ src/components/SplineBackground.tsx
@@ -1,15 +1,5 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const Spline = dynamic(() => import('@splinetool/react-spline'), {
-  ssr: false,
-});
-
 export default function SplineBackground() {
-  return (
-    <div className="fixed inset-0 -z-20">
-      <Spline scene="https://prod.spline.design/ggghIvoFHrhAZTTe/scene.splinecode" />
-    </div>
-  );
+  return null;
 }
